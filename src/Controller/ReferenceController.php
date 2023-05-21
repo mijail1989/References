a7c770cf87e9232fc2cffbabcc7a5e9101f2bf80
mijail1989 <m.surco.developing@gmail.com>
2023-05-21 17:55:58 +0200

    Create Reference

--- src/Controller/ReferenceController.php
+++ src/Controller/ReferenceController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Reference;
 use App\Form\ReferenceType;
 use App\Repository\ReferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,22 +23,21 @@ class ReferenceController extends AbstractController
     }
 
     #[Route('/new', name: 'app_reference_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, ReferenceRepository $referenceRepository): Response
+    public function new(Request $request, ReferenceRepository $referenceRepository,EntityManagerInterface $entityManager): Response
     {
+        $body= json_decode($request->getContent(), true);
         $reference = new Reference();
-        $form = $this->createForm(ReferenceType::class, $reference);
-        $form->handleRequest($request);
+        $reference->setName($body["name"]);
+        $reference->setUrl($body["url"]);
+        $reference->setLang($body["lang"]);
+        $reference->setDescription($body["description"]);
+        $reference->setImg($body["img"]);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $referenceRepository->save($reference, true);
+        $entityManager->persist($reference);
+        $entityManager->flush();
+        
+        return new Response('Saved new product with id '.$reference->getId());
 
-            return $this->redirectToRoute('app_reference_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('reference/new.html.twig', [
-            'reference' => $reference,
-            'form' => $form,
-        ]);
     }
 
     #[Route('/{id}', name: 'app_reference_show', methods: ['GET'])]
