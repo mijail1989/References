69583c65dd6f91ee7859a3c6231a8c5c373748bc
mijail1989 <m.surco.developing@gmail.com>
2023-05-22 18:58:57 +0200

    Find References by User and Id

--- src/Controller/ReferenceController.php
+++ src/Controller/ReferenceController.php
@@ -13,30 +13,32 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+
 #[Route('/api/reference')]
 class ReferenceController extends AbstractController
 {
     public function __construct(
         private Security $security,
-    ){
+    ) {
     }
 
     #[Route('/', name: 'app_reference_index', methods: ['GET'])]
+
     public function index(ReferenceRepository $referenceRepository): Response
     {
-
-        return $this->json($referenceRepository->findAll());
+        $user = $this->security->getUser();
+        return $this->json($referenceRepository->findByUser($user));
     }
 
     #[Route('/new', name: 'app_reference_new', methods: ['GET', 'POST'])]
-    public function new(CreateReferenceRequest $request,EntityManagerInterface $entityManager): Response
+    public function new(CreateReferenceRequest $request, EntityManagerInterface $entityManager): Response
     {
-        
+
         $errors = $request->validate();
-        if(count($errors)){
+        if (count($errors)) {
             return new JsonResponse(['message' => 'Failed'], 403);
         }
-        $body= $request->getRequest()->toArray();
+        $body = $request->getRequest()->toArray();
         $reference = new Reference();
         $reference->setName($body["name"]);
         $reference->setUrl($body["url"]);
@@ -50,11 +52,12 @@ class ReferenceController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_reference_show', methods: ['GET'])]
-    public function show(Reference $reference): Response
+    public function show(ReferenceRepository $referenceRepository,$id): Response
     {
-        return $this->render('reference/show.html.twig', [
-            'reference' => $reference,
-        ]);
+        $user=$this->security->getUser();
+        $referenceId=($referenceRepository->findByUserAndId($user,$id));
+        $status=$referenceId? 200: 403;
+        return new JsonResponse($referenceId,$status);
     }
 
     #[Route('/{id}/edit', name: 'app_reference_edit', methods: ['GET', 'POST'])]
@@ -78,7 +81,7 @@ class ReferenceController extends AbstractController
     #[Route('/{id}', name: 'app_reference_delete', methods: ['POST'])]
     public function delete(Request $request, Reference $reference, ReferenceRepository $referenceRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$reference->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete'. $reference->getId(), $request->request->get('_token'))) {
             $referenceRepository->remove($reference, true);
         }
 
--- src/Repository/ReferenceRepository.php
+++ src/Repository/ReferenceRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Reference;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -39,28 +40,46 @@ class ReferenceRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Reference[] Returns an array of Reference objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('r.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findByUser(User $user): array
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.user = :user')
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getResult();
+    }
+    public function findByUserAndId(User $user,$id)
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.user = :user')
+            ->setParameter('user', $user)
+            ->andWhere('r.id = :id')
+            ->setParameter('id',$id)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+    //    /**
+    //     * @return Reference[] Returns an array of Reference objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('r.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Reference
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Reference
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
