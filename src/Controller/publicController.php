1ca4c5eaf183b6cf5a063fffa0a0ac8af619e049
mijail1989 <m.surco.developing@gmail.com>
2023-05-31 12:26:24 +0200

    return Img url with img content Routes

--- src/Controller/publicController.php
+++ src/Controller/publicController.php
@@ -1,7 +1,22 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-class publicController
+#[Route('/img')]
+class publicController extends AbstractController
 {
+    #[Route('/{img}', name: 'img', methods: ['GET'])]
+    public function index($img): Response
+    {
+        $filename = $this->getParameter('pictures_directory') . '/' . $img;
+        if (file_exists($filename)) {
+            return new BinaryFileResponse($filename);
+        } 
+        return new JsonResponse(['message' => 'Failed'], 403);
+    }
 }
